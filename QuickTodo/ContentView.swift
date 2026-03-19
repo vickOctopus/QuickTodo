64e4820d1a8c7c2722cc4509ce435ca5c3fc7fc2
octopusVick <wangsongquan9@gmail.com>
2026-03-20 00:29:32 +0800

    Initial Commit

---
+++ QuickTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuickTodo
+//
+//  Created by 王崧全 on 2026/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuickTodo/QuickTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTodoApp.swift
+//  QuickTodo
+//
+//  Created by 王崧全 on 2026/3/20.
+//
+
+import SwiftUI
+
+@main
+struct QuickTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
